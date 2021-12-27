29f051eec61e6f7057d149e22895fd27b87983d6
Soumyajit Das <mepioneiro@gmail.com>
2021-12-28 01:14:06 +0530

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      0: "#03071e",
+      1: "#370617",
+      2: "#6a040f",
+      3: "#9d0208",
+      4: "#d00000",
+      5: "#dc2f02",
+      6: "#e85d04",
+      7: "#f48c06",
+      8: "#faa307",
+      9: "#ffba08",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
