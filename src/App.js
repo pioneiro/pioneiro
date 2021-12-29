3cf7721b21fc9749b1d769a3118effe7ba539cae
Soumyajit Das <mepioneiro@gmail.com>
2021-12-29 17:27:08 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return <div>Portfolio</div>;
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {},
+    extend: {},
+  },
+  plugins: [],
+};
